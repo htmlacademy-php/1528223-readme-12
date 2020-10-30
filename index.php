18247e7dda4d3110012922e7503c32e212856561
Максим Филимонов <maximfilimonov@yandex.ru>
2020-10-30 19:17:05 +0300

    заменил старый синтаксис определения массивов на новый

--- index.php
+++ index.php
@@ -210,37 +210,38 @@ $user_name = 'Максим'; // укажите здесь ваше имя
             
 			<?php
 			// создаём двумерный массив
-			$popular_posts = array (	array (	"header" 	=> "Цитата",
-												"type"		=> "post-quote",
-												"content"	=> "Мы в жизни любим только раз, а после ищем лишь похожих",
-												"username"	=> "Лариса",
-												"avatar"	=> "userpic-larisa-small.jpg"
-												),
-										array (	"header" 	=> "Игра престолов",
-												"type"		=> "post-text",
-												"content"	=> "Не могу дождаться начала финального сезона своего любимого сериала!",
-												"username"	=> "Владик",
-												"avatar"	=> "userpic.jpg"
-												),
-										array (	"header" 	=> "Наконец, обработал фотки!",
-												"type"		=> "post-photo",
-												"content"	=> "rock-medium.jpg",
-												"username"	=> "Виктор",
-												"avatar"	=> "userpic-mark.jpg"
-												),
-										array (	"header" 	=> "Моя мечта",
-												"type"		=> "post-photo",
-												"content"	=> "coast-medium.jpg",
-												"username"	=> "Лариса",
-												"avatar"	=> "userpic-larisa-small.jpg"
-												),
-										array (	"header" 	=> "Лучшие курсы",
-												"type"		=> "post-link",
-												"content"	=> "www.htmlacademy.ru",
-												"username"	=> "Владик",
-												"avatar"	=> "userpic.jpg"
-												)
-										);
+			$popular_posts = [
+								[	"header" 	=> "Цитата",
+									"type"		=> "post-quote",
+									"content"	=> "Мы в жизни любим только раз, а после ищем лишь похожих",
+									"username"	=> "Лариса",
+									"avatar"	=> "userpic-larisa-small.jpg"
+								],
+								[	"header" 	=> "Игра престолов",
+									"type"		=> "post-text",
+									"content"	=> "Не могу дождаться начала финального сезона своего любимого сериала!",
+									"username"	=> "Владик",
+									"avatar"	=> "userpic.jpg"
+								],
+								[	"header" 	=> "Наконец, обработал фотки!",
+									"type"		=> "post-photo",
+									"content"	=> "rock-medium.jpg",
+									"username"	=> "Виктор",
+									"avatar"	=> "userpic-mark.jpg"
+								],
+								[	"header" 	=> "Моя мечта",
+									"type"		=> "post-photo",
+									"content"	=> "coast-medium.jpg",
+									"username"	=> "Лариса",
+									"avatar"	=> "userpic-larisa-small.jpg"
+								],
+								[	"header" 	=> "Лучшие курсы",
+									"type"		=> "post-link",
+									"content"	=> "www.htmlacademy.ru",
+									"username"	=> "Владик",
+									"avatar"	=> "userpic.jpg"
+								]
+							];
 			?>
             
             <?php foreach($popular_posts as $key => $val): ?>

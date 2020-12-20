2e74acaa5e7b617f831f67cc2926634bd9b07636
Максим Филимонов <maximfilimonov@yandex.ru>
2020-12-20 15:35:44 +0300

    исправил ошибки

--- schema.sql
+++ schema.sql
@@ -12,9 +12,9 @@ CREATE TABLE users(
 )
 COMMENT='Таблица пользователей';
 
-CREATE UNIQUE INDEX users.emails ON users(email);
+CREATE UNIQUE INDEX users_emails ON users(email);
 
-CREATE TABLE hashtag(
+CREATE TABLE hashtags(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	hashtag VARCHAR(64) NOT NULL UNIQUE
 )
@@ -24,7 +24,7 @@ CREATE TABLE content_type(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(16) NOT NULL UNIQUE,
 	class VARCHAR(16) NOT NULL UNIQUE
-}
+)
 COMMENT='Таблица типов контента';
 
 CREATE TABLE messages(
@@ -37,17 +37,16 @@ CREATE TABLE messages(
 		ON UPDATE CASCADE,
 	FOREIGN KEY (recepient_id)
 		REFERENCES users(id)
-		ON UPDATE CASCADE,
-	PRIMARY KEY (sender_id, recepient_id)
+		ON UPDATE CASCADE
 )
 COMMENT='Таблица личных сообщений';
 
 CREATE UNIQUE INDEX communication ON messages(sender_id, recepient_id);
 
-CREATE TABLE post(
+CREATE TABLE posts(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	text TEXT;
+	text TEXT,
 	author VARCHAR(128),
 	image_url VARCHAR(128),
 	video_url VARCHAR(32),
@@ -62,21 +61,21 @@ CREATE TABLE post(
 	FOREIGN KEY (content_type_id)
 		REFERENCES content_type(id)
 		ON UPDATE CASCADE 
-		ON DELETE CASCADE,
+		ON DELETE CASCADE
 )
 COMMENT='Таблица постов -- связь с хештегами вынесена в отдельную таблицу ниже';
 
-CREATE INDEX posts_author ON post(user_id);
+CREATE INDEX posts_author ON posts(user_id);
 
 CREATE TABLE post_hashtag(
 	post_id INT,
 	hashtag_id INT,
 	FOREIGN KEY (post_id)
-		REFERENCES post(id)
+		REFERENCES posts(id)
 		ON UPDATE CASCADE 
 		ON DELETE CASCADE,
 	FOREIGN KEY (hashtag_id)
-		REFERENCES hashtag(id)
+		REFERENCES hashtags(id)
 		ON UPDATE CASCADE 
 		ON DELETE CASCADE,
 	PRIMARY KEY (post_id, hashtag_id)
@@ -89,11 +88,11 @@ CREATE TABLE comments(
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id)
-		REFERENCES user(id)
+		REFERENCES users(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
 	FOREIGN KEY (post_id)
-		REFERENCES post(id)
+		REFERENCES posts(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 )
@@ -105,11 +104,11 @@ CREATE TABLE subscribes(
 	sourcer_id INT,
 	subscriber_id INT,
 	FOREIGN KEY (sourcer_id)
-		REFERENCES user(id)
+		REFERENCES users(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
 	FOREIGN KEY (subscriber_id)
-		REFERENCES user(id)
+		REFERENCES users(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
 	PRIMARY KEY (sourcer_id, subscriber_id)
@@ -122,10 +121,10 @@ CREATE TABLE likes(
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id)
-		REFERENCES user(id)
+		REFERENCES users(id)
 		ON UPDATE CASCADE,
 	FOREIGN KEY (post_id)
-		REFERENCES post(id)
+		REFERENCES posts(id)
 		ON UPDATE CASCADE,
 	PRIMARY KEY (user_id, post_id)
 )
